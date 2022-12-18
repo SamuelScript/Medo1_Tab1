e5a6becb078ab08a69561751e59aedb9cadd625a
Samuel <samuelbernabefarias@gmail.com>
2022-12-18 17:49:16 -0300

    Implementado algoritimo de problema de contorno.

--- src/Main.java
+++ src/Main.java
@@ -21,3 +21,22 @@ public class Main {
 
     }
 }
+
+class Trabalho {
+    public static void t1part1(GChart g) {
+        RungeKutta3Double runge = new RungeKutta3Double(0.0, 0.01, 1.0, 10.0, 0.0, 10.0, 10.0, 10.0, 10.0);
+        runge.run(g);
+    }
+    public static void t1part2(GChart g) {
+        int nodes = 7;
+        MalhaComputacional mcomp = new MalhaComputacional(5.0/(nodes-1),(27)*(0.000001),5.0,0.1,0.0,(9)*(0.000001));
+        double[][] matrix = mcomp.gerar_matriz_A(nodes-2);//Número de nós internos
+        double[] vetor_solucao = mcomp.gerar_vetor_b(nodes-2);
+        TDM tdm = new TDM(matrix,vetor_solucao);
+        tdm.solve();
+        g.addData(0,0,0.1);
+        for(int i = 0; i < nodes - 2; i++) g.addData(0, i+1, vetor_solucao[i]);
+        g.addData(0,nodes - 1,0.0);
+        g.setLabel(0,"Pontos da malha ao longo de L");
+    }
+}
--- src/MalhaComputacional.java
+++ src/MalhaComputacional.java
@@ -1,39 +1,35 @@
 public class MalhaComputacional {
     private double dx;
-    private double h;
     private double L;
-    private double Ta;
     private double Te;
     private double Td;
+    private double D;
+    private double h;
 
-    public MalhaComputacional( double dx, double h,double L,double Ta, double Te,double Td) {
+    public MalhaComputacional(double dx, double h,double L, double Te,double Td, double D) {
         this.dx = dx;
         this.h = h;
         this.L = L;
-        this.Ta = Ta;
         this.Te = Te;
         this.Td = Td;
+        this.D = D;
     }
     public double[][] gerar_matriz_A(int n) {
         double[][] A = new double[n][n];
         for(int i = 0; i < n; i++) {
-            A[i][i] = 2 + Math.pow(dx,2)*h;
+            A[i][i] = 2*D + Math.pow(dx,2)*h;
             if(i+1 < n){
-                A[i+1][i] = -1;
-                A[i][i+1] = -1;
+                A[i+1][i] = -D;
+                A[i][i+1] = -D;
             }
         }
         return A;
     }
     public double[] gerar_vetor_b(int n) {
         double[] b = new double[n];
-        for(int i = 0; i < n; i++) {
-            b[i] = Math.pow(dx,2)*h*Ta;
-            if(i%n == 0) b[i] += (i==0?Te:Td);
-        }
+        for(int i = 1; i < n-1; i++) b[i] = 0;
+        b[0] += D*Te;
+        b[n-1] += D*Td;
         return b;
-    }
-    public void resolver_sistema(double[] Ti ) {
-
     }
 }
